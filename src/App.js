87d47599841eb8492583753cba87c0578629ec15
shawnm4 <shawnm18057310@gmail.com>
2023-11-09 22:56:51 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+import { useState } from "react";
+
+// import './App.css';
+export default App;
 
 function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const [increment, setIncrement] = useState(1);
+  const [countDis, setcountDis] = useState(0);
+
+  function addNum() {
+    setcountDis((count) => countDis + increment);
+  }
+
+  function subNum() {
+    setcountDis((count) => countDis - increment);
+  }
+
+  function changeIncrementUp() {
+    setIncrement((i) => i + 1);
+  }
+  function changeIncrementDown() {
+    setIncrement((i) => i - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>
+          {" "}
+          <button onClick={subNum}>-</button>{" "}
+        </span>
+        <span>Subtract Or Add</span>
+        <span>
+          <button onClick={addNum}> + </button>{" "}
+        </span>
+      </div>
+      <div>
+        <span>
+          <button onClick={changeIncrementDown}>-</button>
+        </span>
+        <span>Increment:{increment} </span>
+        <span>
+          <button onClick={changeIncrementUp}>+</button>
+        </span>
+      </div>
+
+      <p> {countDis}</p>
     </div>
   );
 }
-
-export default App;
